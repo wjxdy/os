b34ea533532ee8311b5c64a3302dad1e66d14cf7
Daye <2077152374@qq.com>
2026-08-02 15:20:58 +0800

    feat: print first kernel serial message

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -2,18 +2,28 @@
 #![no_main]
 
 use bootloader_api::{BootInfo, entry_point};
-use core::panic::PanicInfo;
+use core::{fmt::Write, panic::PanicInfo};
 
 entry_point!(kernel_main);
 
+fn serial_port() -> uart_16550::SerialPort {
+    let mut port = unsafe { uart_16550::SerialPort::new(0x3F8) };
+    port.init();
+    port
+}
+
 fn kernel_main(_boot_info: &'static mut BootInfo) -> ! {
+    let mut serial = serial_port();
+    writeln!(serial, "Rust Os: kernel entered").expect("failed to write to COM1");
     loop {
         core::hint::spin_loop();
     }
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    let _ = writeln!(serial_port(), "KERNEL PANIC: {info}");
+
     loop {
         core::hint::spin_loop();
     }
